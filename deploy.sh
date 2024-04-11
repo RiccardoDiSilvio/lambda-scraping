e5008ddd35b30947523ddf7bfb7c11914018bcf7
Riccardo Di Silvio <163587604+RiccardoDiSilvio@users.noreply.github.com>
2024-04-11 13:27:40 -0500

    switching to us-east-2

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@
 echo "command - npx tsc"
 npx tsc
 
-echo "command - aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 637423352927.dkr.ecr.us-west-2.amazonaws.com"
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 637423352927.dkr.ecr.us-west-2.amazonaws.com
+echo "command - aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 637423352927.dkr.ecr.us-east-2.amazonaws.com"
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 637423352927.dkr.ecr.us-east-2.amazonaws.com
 
 echo "command - docker build --platform linux/amd64 --progress=plain -t scraper ."
 docker build --platform linux/amd64 --progress=plain -t scraper .
 
-echo "command - docker tag scraper:latest 637423352927.dkr.ecr.us-west-2.amazonaws.com/scraper:latest"
-docker tag scraper:latest 637423352927.dkr.ecr.us-west-2.amazonaws.com/scraper:latest
+echo "command - docker tag scraper:latest 637423352927.dkr.ecr.us-east-2.amazonaws.com/scraper:latest"
+docker tag scraper:latest 637423352927.dkr.ecr.us-east-2.amazonaws.com/scraper:latest
 
-echo "command - docker push 637423352927.dkr.ecr.us-west-2.amazonaws.com/scraper:latest"
-docker push 637423352927.dkr.ecr.us-west-2.amazonaws.com/scraper:latest
+echo "command - docker push 637423352927.dkr.ecr.us-east-2.amazonaws.com/scraper:latest"
+docker push 637423352927.dkr.ecr.us-east-2.amazonaws.com/scraper:latest
